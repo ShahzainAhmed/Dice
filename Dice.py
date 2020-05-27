356679c120321f88c64b0c461e5597e71c3a75f7
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2020-05-28 00:56:12 +0500

    Final commit

--- Dice.py
+++ Dice.py
@@ -6,7 +6,7 @@ import random
 min = 1 # Minimum value is set to 1.
 max = 6 # Maximum value is set to 2.  
 
-roll_again = "yes"  # Initializing a variable.
+roll_again = "yes"    # Initializing a variable.
 
 while roll_again == "yes" or roll_again == "y":     # Using while statement
     print ("Rolling the dices...")                  # Using print statements.
