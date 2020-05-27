daffad545173ef249d172f5806432165ef9cf8df
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2020-05-28 00:54:02 +0500

    s

--- Dice.py
+++ Dice.py
@@ -12,9 +12,4 @@ while roll_again == "yes" or roll_again == "y":     # Using while statement
     print (random.randint(min, max))
     print (random.randint(min, max))
 
-    roll_again = input("Roll the dices again?")
-    
-    
-    
-    
-    
+    roll_again = input("Roll the dices again?") 
